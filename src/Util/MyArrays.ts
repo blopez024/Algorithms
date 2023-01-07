668aa2d3b1190d866440280796de2822f729e73f
blopez024 <bmlopez024@gmail.com>
2023-01-06 20:54:06 -0600

    init typescipt build

--- src/Util/MyArrays.ts
+++ src/Util/MyArrays.ts
@@ -1,16 +1,19 @@
+/**
+ * @file Utility to create arrays to be used in algorithms.
+ * @author BML 
+ */
 
 /**
  * Creates a new array with the specificized amount of elements within a range.
  * The array can be return in sorted order.
  * 
- *
  * @param {number} [nums=10]    - number of elements in the array
  * @param {number} [min=0]      - element in array is no lower than min
  * @param {number} [max=100]    - element in array is no higher than max - 1
  * @param {boolean} [ordered=false] - whether the array should be returned sorted
  * @return {number[]}           - returns a new array 
  */
-function randomArray(nums: number = 10, min: number = 0, max: number = 100, ordered: boolean =false): number[] {
+function randomArray(nums: number = 10, min: number = 0, max: number = 100, ordered: boolean = false): number[] {
     let arr: number[] = [];
 
     while (nums > 0) {
@@ -24,6 +27,5 @@ function randomArray(nums: number = 10, min: number = 0, max: number = 100, orde
     }
     return arr;
 }
-
 
 export { randomArray };
